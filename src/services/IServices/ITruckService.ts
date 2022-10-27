44d541713e96d178ae70921f15d0aaa17f10ba2c
Marta Portugal <1201845@isep.ipp.pt>
2022-10-27 19:07:59 +0100

    truck service add

---
+++ src/services/IServices/ITruckService.ts
@@ -0,0 +1,9 @@
+import { Result } from "../../core/logic/Result";
+import ITruckDTO from "../../dto/ITruckDTO";
+
+export default interface ITruckService  {
+  createTruck(truckDTO: ITruckDTO): Promise<Result<ITruckDTO>>;
+  updateTruck(truckDTO: ITruckDTO): Promise<Result<ITruckDTO>>;
+
+  getLicencePlate (licencePlate: string): Promise<Result<ITruckDTO>>;
+}
---
+++ src/services/TruckService.ts
@@ -0,0 +1,77 @@
+import { Service, Inject } from 'typedi';
+import config from "../../config";
+import ITruckDTO from '../dto/ITruckDTO';
+import { Truck } from "../domain/Truck";
+import ITruckRepo from './IRepos/ITruckRepo';
+import ITruckService from './IServices/ITruckService';
+import { Result } from "../core/logic/Result";
+import { TruckMap } from "../mappers/TruckMap";
+
+@Service()
+export default class TruckService implements ITruckService {
+  constructor(
+      @Inject(config.repos.Truck.name) private truckRepo : ITruckRepo
+  ) {}
+
+  public async getLicencePlate( licencePlate: string): Promise<Result<ITruckDTO>> {
+    try {
+      const truck = await this.truckRepo.findByLicencePlate(licencePlate);
+
+      if (truck === null) {
+        return Result.fail<ITruckDTO>("Truck not found");
+      }
+      else {
+        const truckDTOResult = TruckMap.toDTO( truck ) as ITruckDTO;
+        return Result.ok<ITruckDTO>( truckDTOResult )
+        }
+    } catch (e) {
+      throw e;
+    }
+  }
+
+
+  public async createTruck(truckDTO: ITruckDTO): Promise<Result<ITruckDTO>> {
+    try {
+
+      const truckOrError = await Truck.create( truckDTO );
+
+      if (truckOrError.isFailure) {
+        return Result.fail<ITruckDTO>(truckOrError.errorValue());
+      }
+
+      const truckResult = truckOrError.getValue();
+
+      await this.truckRepo.save(truckResult);
+
+      const truckDTOResult = TruckMap.toDTO( truckResult ) as ITruckDTO;
+      return Result.ok<ITruckDTO>( truckDTOResult )
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  public async updateTruck(truckDTO: ITruckDTO): Promise<Result<ITruckDTO>> {
+    try {
+      const truck = await this.truckRepo.findByLicencePlate(truckDTO.licencePlate);
+
+      if (truck === null) {
+        return Result.fail<ITruckDTO>("Truck not found");
+      }
+      else {
+        truck.tare = truckDTO.tare;
+        truck.capacity = truckDTO.capacity;
+        truck.maxBateryCapacity = truckDTO.maxBateryCapacity;
+        truck.autonomyFullChargeLoad = truckDTO.autonomyFullChargeLoad;
+        truck.timeCharging = truckDTO.timeCharging; 
+                
+        await this.truckRepo.save(truck);
+
+        const truckDTOResult = TruckMap.toDTO( truck ) as ITruckDTO;
+        return Result.ok<ITruckDTO>( truckDTOResult )
+        }
+    } catch (e) {
+      throw e;
+    }
+  }
+
+}
