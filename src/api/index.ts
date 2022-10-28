f5c84cf32aabb34f45f41ce488b9480cb3530659
João Tiago Araújo <1200584@isep.ipp.pt>
2022-10-28 12:19:23 +0100

    updated index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import auth from './routes/userRoute';
 import user from './routes/userRoute';
 import role from './routes/roleRoute';
+import route from './routes/routeRoute';
 
 export default () => {
 	const app = Router();
@@ -10,5 +11,6 @@ export default () => {
 	user(app);
 	role(app);
 	
+	route(app);
 	return app
 }
