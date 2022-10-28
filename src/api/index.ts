272b9b8378a0443c99b1201c3d18546148ee6a63
Marta Portugal <1201845@isep.ipp.pt>
2022-10-28 12:01:13 +0100

    truckroute

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import auth from './routes/userRoute';
 import user from './routes/userRoute';
 import role from './routes/roleRoute';
+import truck from './routes/truckRoute';
 
 export default () => {
 	const app = Router();
@@ -9,6 +10,7 @@ export default () => {
 	auth(app);
 	user(app);
 	role(app);
+	truck(app);
 	
 	return app
 }
