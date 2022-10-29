7b49ca080af48cacaa0fd7d8e3577f0658996624
João Tiago Araújo <1200584@isep.ipp.pt>
2022-10-30 00:34:37 +0100

    updated routeId.ts

--- src/domain/routeId.ts
+++ src/domain/routeId.ts
@@ -1,6 +1,15 @@
 import { UniqueEntityID } from "../core/domain/UniqueEntityID";
+import { ValueObject } from "../core/domain/ValueObject";
 
-export class RouteId extends UniqueEntityID {
+interface RouteProps {
+    rId: string;
+  }
 
+export class RouteId extends ValueObject<RouteProps> {
+get rId(): string {
+    return this.props.rId;
+}
+
+//private constructor(props: RouteProps) 
     
 }
