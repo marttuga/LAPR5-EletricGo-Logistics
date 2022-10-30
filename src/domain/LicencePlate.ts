1973d99bb0676af28884fef636e125ffad226b6a
Marta Portugal <1201845@isep.ipp.pt>
2022-10-30 23:53:56 +0000

    licence palte added method

--- src/domain/LicencePlate.ts
+++ src/domain/LicencePlate.ts
@@ -1,17 +1,29 @@
+//import { UniqueEntityID } from "../core/domain/UniqueEntityID";
 import { ValueObject } from "../core/domain/ValueObject";
+import { Guard } from "../core/logic/Guard";
+import { Result } from "../core/logic/Result";
 
-   
-    interface TruckProps {
-        licencePlate: string;
-      }
-    
-    export class LicencePlate extends ValueObject<TruckProps> {
-    get licencePlate(): string {
-        return this.props.licencePlate;
+interface TruckProps {
+    licencePlate: string;
+  }
+
+export class LicencePlate extends ValueObject<TruckProps> {
+get licencePlate(): string {
+    return this.props.licencePlate;
+}
+
+private constructor(props: TruckProps) {
+    super(props);
+}
+
+public static create(id: string): Result<LicencePlate> {
+    const guardResult = Guard.againstNullOrUndefined(id, 'id');
+
+    if (!guardResult.succeeded) {
+      return Result.fail<LicencePlate>(guardResult.message);
+    } else {
+      return Result.ok<LicencePlate>(new LicencePlate({ licencePlate: id }));
     }
-    
-    private constructor(props: TruckProps) {
-        super(props);
-    }
+  }
     
 }
