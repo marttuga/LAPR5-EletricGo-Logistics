ac730bccd6430f56db75dca76a9d8b0945451e97
Marta Portugal <1201845@isep.ipp.pt>
2022-10-31 09:23:42 +0000

    truck repo chnages

--- src/services/IRepos/ITruckRepo.ts
+++ src/services/IRepos/ITruckRepo.ts
@@ -3,7 +3,6 @@ import { Truck } from "../../domain/Truck";
 import { LicencePlate } from "../../domain/LicencePlate";
 
 export default interface ITruckRepo extends Repo<Truck> {
-	exists(truck: Truck): Promise<boolean>;
 	save(Truck: Truck): Promise<Truck>;
 	findByLicencePlate (LicencePlate: LicencePlate | string): Promise<Truck>;
 }
