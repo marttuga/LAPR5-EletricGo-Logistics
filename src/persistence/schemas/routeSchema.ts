5010091d044f97df434ee7df01ccb86b84b068d3
João Tiago Araújo <1200584@isep.ipp.pt>
2022-10-31 11:32:45 +0000

    updated routeSchema.ts

--- src/persistence/schemas/routeSchema.ts
+++ src/persistence/schemas/routeSchema.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 const RouteSchema = new mongoose.Schema(
   {
-    routeId: { type: Number, unique: true },
+    routeId: { type: String, unique: true },
     distance: {
         type: Number,
         required: [true, 'Please enter distance'],
