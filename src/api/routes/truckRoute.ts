03c069b5949aa1e6a5393178233630c23ae5e2b9
Marta Portugal <1201845@isep.ipp.pt>
2022-11-02 10:47:47 +0000

    truck route alteration

--- src/api/routes/truckRoute.ts
+++ src/api/routes/truckRoute.ts
@@ -1,7 +1,7 @@
-import { NextFunction, Request, Response, Router } from 'express';
+import { Router } from 'express';
 import { celebrate, Joi } from 'celebrate';
 
-import { Container } from 'typedi';
+import { Container, Service } from 'typedi';
 import ITruckController from '../../controllers/IControllers/ITruckController'; 
 
 import config from "../../../config";
@@ -12,30 +12,6 @@ export default (app: Router) => {
   app.use('/truck', truck);
 
   const ctrl = Container.get(config.controllers.truck.name) as ITruckController;
-
-/* 
-  truck.get('truck',
-    celebrate({
-      body: Joi.object({
-        tare: Joi.number().required(),
-        capacity:Joi.number().required(),
-        maxBateryCapacity:Joi.number().required(),
-        autonomyFullChargeLoad:Joi.number().required(),
-        timeCharging: Joi.number().required()
-      }),
-    }),
-  async (req:Request, res: Response,next: NextFunction) => {
-  try {
-    const result=await ctrl.get(req,res,next);
-    return res.json(result);
-    
-  } catch (error) {
-    next(error);
-    
-  }
-    
-  },
-); */
 
 truck.post('/createTruck',
 celebrate({
