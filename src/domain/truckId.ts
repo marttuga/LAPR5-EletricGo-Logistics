9e0bf9c56123c3cd98e7ab682674ea3770e32e20
Marta Portugal <1201845@isep.ipp.pt>
2022-11-02 13:13:04 +0000

    truck id alteration

--- src/domain/truckId.ts
+++ src/domain/truckId.ts
@@ -5,8 +5,11 @@ import { Result } from "../core/logic/Result"; */
 
 import { Entity } from "../core/domain/Entity";
 import { UniqueEntityID } from "../core/domain/UniqueEntityID";
+import { ValueObject } from "../core/domain/ValueObject";
+import { Guard } from "../core/logic/Guard";
+import { Result } from "../core/logic/Result";
 
-export class TruckId extends Entity<any> {
+/* export class TruckId extends Entity<any> {
 
 	get id (): UniqueEntityID {
 		return this._id;
@@ -15,30 +18,30 @@ export class TruckId extends Entity<any> {
 	private constructor (id?: UniqueEntityID) {
 		super(null, id)
 	}
-}
+} */
 
-/* interface TruckProps {
-    licencePlate: string;
+ interface TruckProps {
+    truckId: string;
   }
 
-export class LicencePlate extends ValueObject<TruckProps> {
-get licencePlate(): string {
-    return this.props.licencePlate;
+export class TruckId extends ValueObject<TruckProps> {
+get TruckId(): string {
+    return this.props.truckId;
 }
 
 private constructor(props: TruckProps) {
     super(props);
 }
 
-public static create(id: string): Result<LicencePlate> {
+public static create(id: string): Result<TruckId> {
     const guardResult = Guard.againstNullOrUndefined(id, 'id');
 
     if (!guardResult.succeeded) {
-      return Result.fail<LicencePlate>(guardResult.message);
+      return Result.fail<TruckId>(guardResult.message);
     } else {
-      return Result.ok<LicencePlate>(new LicencePlate({ licencePlate: id }));
+      return Result.ok<TruckId>(new TruckId({ truckId: id }));
     }
   }
     
-} */
+} 
 
