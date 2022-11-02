0720ad1855e3207a54d9c1a426aa1946bcaa9154
João Tiago Araújo <1200584@isep.ipp.pt>
2022-11-02 09:41:28 +0000

    updated get method truck Controllers

--- src/controllers/IControllers/ITruckController.ts
+++ src/controllers/IControllers/ITruckController.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
 export default interface ITruckController  {
-  get(req: Request, res: Response, next: NextFunction): void;
+  getTruck(req: Request, res: Response, next: NextFunction);
   createTruck(req: Request, res: Response, next: NextFunction);
   updateTruck(req: Request, res: Response, next: NextFunction);
   getTruckId(req: Request, res: Response, next: NextFunction);
--- src/controllers/TruckController.ts
+++ src/controllers/TruckController.ts
@@ -15,9 +15,9 @@ export default class TruckController implements ITruckController /* TODO: extend
   ) {}
 
   
-  public async get(req: Request, res: Response, next: NextFunction) {
+  public async getTruck(req: Request, res: Response, next: NextFunction) {
     try {
-      const truckOrError = await this.truckServiceInstance.getTruckId(req.body as string) as Result<ITruckDTO>;
+      const truckOrError = await this.truckServiceInstance.getTrucks() as Result<ITruckDTO>;
 
       if (truckOrError.isFailure) {
         return res.status(404).send();
@@ -33,7 +33,6 @@ export default class TruckController implements ITruckController /* TODO: extend
   public async createTruck(req: Request, res: Response, next: NextFunction) {
     try {
       const truckOrError = await this.truckServiceInstance.createTruck(req.body as ITruckDTO) as Result<ITruckDTO>;
-        
       if (truckOrError.isFailure) {
         return res.status(402).send();
       }
