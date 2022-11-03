0b5bbb1bb337e80de8659751c7911ef5a883b423
Marta Portugal <1201845@isep.ipp.pt>
2022-11-03 10:39:39 +0000

    value objects alterations

--- src/dataschema/ITruckPersistence.ts
+++ src/dataschema/ITruckPersistence.ts
@@ -1,11 +1,16 @@
+import { AutonomyFullChargeLoad } from "../domain/autonomyFullChargeLoad";
+import { Capacity } from "../domain/capacity";
 import { LicencePlate } from "../domain/licencePlate";
+import { MaxBateryCapacity } from "../domain/maxBateryCapacity";
+import { Tare } from "../domain/tare";
+import { TimeCharging } from "../domain/timeCharging";
 
 export interface ITruckPersistence {
 	_id: string;
 	licencePlate:LicencePlate;
-	tare: number;
-	capacity:number;
-	maxBateryCapacity:number;
-	autonomyFullChargeLoad:number;
-	timeCharging: number;
+	tare: Tare;
+	capacity:Capacity;
+	maxBateryCapacity:MaxBateryCapacity;
+	autonomyFullChargeLoad:AutonomyFullChargeLoad;
+	timeCharging: TimeCharging;
   }
