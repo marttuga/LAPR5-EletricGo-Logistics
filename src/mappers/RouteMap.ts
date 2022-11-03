1c389574b004ac73e9ffe406907612e48620220b
João Tiago Araújo <1200584@isep.ipp.pt>
2022-11-03 21:23:39 +0000

    updated route classes

--- src/mappers/RouteMap.ts
+++ src/mappers/RouteMap.ts
@@ -15,12 +15,13 @@ export class RouteMap extends Mapper<Route> {
   public static toDTO( route: Route): IRouteDTO {
     return {
       //id: Route.id.toString(),
+      routeId: route.routeId.routeId,
       distance: route.distance,
       routeTime: route.routeTime,
       batteryWaste: route.batteryWaste,
       arrivalId: route.arrivalId,
       departureId: route.departureId,
-
+      extraTime: route.extraTime
     } as IRouteDTO;
   }
 
@@ -29,12 +30,13 @@ export class RouteMap extends Mapper<Route> {
     const routeId = await repo.findByRouteId(raw.routeId);
 
     const RouteOrError = Route.create({
-      routeId: raw.routeId,
+      routeId: raw.routeId.routeId,
       distance: raw.distance,
       routeTime: raw.routeTime,
       batteryWaste: raw.batteryWaste,
       arrivalId: raw.arrivalId,
       departureId: raw.departureId,
+      extraTime: raw.extraTime
     }, new UniqueEntityID(raw.routeId))
 
     RouteOrError.isFailure ? console.log(RouteOrError.error) : '';
@@ -45,11 +47,13 @@ export class RouteMap extends Mapper<Route> {
   public static toPersistence (Route: Route): any {
     const a = {
       domainId: Route.id.toString(),
+      routeId: Route.routeId.routeId,
       distance: Route.distance,
       routeTime: Route.routeTime,
       batteryWaste: Route.batteryWaste,
       arrivalId: Route.arrivalId,
-      departureId: Route.departureId
+      departureId: Route.departureId,
+      extraTime: Route.extraTime
     }
     return a;
   }
