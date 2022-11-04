3322fb68145dd46d378f1fd218fb4b8f39123abf
João Tiago Araújo <1200584@isep.ipp.pt>
2022-11-04 18:02:02 +0000

    updated routeRoute

--- src/api/routes/routeRoute.ts
+++ src/api/routes/routeRoute.ts
@@ -2,18 +2,18 @@ import { Router } from 'express';
 import { celebrate, Joi } from 'celebrate';
 
 import { Container, Service } from 'typedi';
-import IRouteController from '../../controllers/IControllers/IRouteController'; 
+import IRouteController from '../../controllers/IControllers/IRouteController';
 
-import config from "../../../config";
+import config from '../../../config';
 
 const route = Router();
-
 export default (app: Router) => {
   app.use('/routes', route);
 
   const ctrl = Container.get(config.controllers.route.name) as IRouteController;
 
-  route.post('/createRoute',
+  route.post(
+    '/createRoute',
     celebrate({
       body: Joi.object({
         routeId: Joi.string().required(),
@@ -22,26 +22,27 @@ export default (app: Router) => {
         batteryWaste: Joi.number().required(),
         arrivalId: Joi.string().required(),
         departureId: Joi.string().required(),
-        extraTime : Joi.number().required()
-
-      })
-    }),
-    (req, res, next) => ctrl.createRoute(req, res, next) );
-
-  route.put('/updateRoute',
-    celebrate({
-      body: Joi.object({
-        routeId: Joi.string().required(),
-        distance: Joi.number().required(),
-        routeTime: Joi.number().required(),
-        batteryWaste: Joi.number().required(),
-        arrivalId: Joi.string().required(),
-        departureId: Joi.string().required(),
-        extraTime : Joi.number().required()
+        extraTime: Joi.number().required(),
       }),
     }),
-    (req, res, next) => ctrl.updateRoute(req, res, next) );
+    (req, res, next) => ctrl.createRoute(req, res, next),
+  );
 
-    route.get('/getRoute',
-    (req,res,next) => ctrl.getRoute(req, res, next) );
-}
+  route.put(
+    '/updateRoute',
+    celebrate({
+      body: Joi.object({
+        routeId: Joi.string().required(),
+        distance: Joi.number().required(),
+        routeTime: Joi.number().required(),
+        batteryWaste: Joi.number().required(),
+        arrivalId: Joi.string().required(),
+        departureId: Joi.string().required(),
+        extraTime: Joi.number().required(),
+      }),
+    }),
+    (req, res, next) => ctrl.updateRoute(req, res, next),
+  );
+
+  route.get('/getRoute', (req, res, next) => ctrl.getRoute(req, res, next));
+};
