f0619ace642100df8e162f2535b6b5562d2d9c98
João Tiago Araújo <1200584@isep.ipp.pt>
2022-11-04 11:26:45 +0000

    updated IRouteDto

--- src/dto/IRouteDTO.ts
+++ src/dto/IRouteDTO.ts
@@ -1,11 +1,16 @@
+import { BatteryWaste } from "../domain/batteryWaste";
+import { Distance } from "../domain/distance";
+import { ExtraTime } from "../domain/extraTime";
+import { RouteId } from "../domain/routeId";
+import { RouteTime } from "../domain/routeTime";
 
 export default interface IRouteDTO {
-    routeId: string;
-    distance: number,
-    routeTime: number,
-    batteryWaste: number,
+    routeId: RouteId;
+    distance: Distance,
+    routeTime: RouteTime,
+    batteryWaste: BatteryWaste,
     arrivalId: string,
     departureId: string,
-    extraTime: number,
+    extraTime: ExtraTime,
   }
   
