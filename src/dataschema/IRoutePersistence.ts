c8e3ae9a6406fbd6c5158391085d4d1723745921
João Tiago Araújo <1200584@isep.ipp.pt>
2022-11-04 11:26:33 +0000

    updated IRoutePersistence

--- src/dataschema/IRoutePersistence.ts
+++ src/dataschema/IRoutePersistence.ts
@@ -1,10 +1,16 @@
+import { BatteryWaste } from "../domain/batteryWaste";
+import { Distance } from "../domain/distance";
+import { ExtraTime } from "../domain/extraTime";
+import { RouteId } from "../domain/routeId";
+import { RouteTime } from "../domain/routeTime";
 
 export interface IRoutePersistence {
-    routeId: string;
-    distance: number;
-    routeTime: number;
-    batteryWaste: number;
+    _id: string;
+    routeId: RouteId;
+    distance: Distance;
+    routeTime: RouteTime;
+    batteryWaste: BatteryWaste;
     arrivalId: string;
     departureId: string;
-    extraTime: number;
+    extraTime: ExtraTime;
   }
