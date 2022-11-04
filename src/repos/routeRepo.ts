bb042c14be2950f5bb093508abc804606f7f1de8
João Tiago Araújo <1200584@isep.ipp.pt>
2022-11-04 18:02:46 +0000

    updated route repos

--- src/repos/routeRepo.ts
+++ src/repos/routeRepo.ts
@@ -1,9 +1,9 @@
 import { Service, Inject } from 'typedi';
 
-import IRouteRepo from "../services/IRepos/IRouteRepo";
-import { Route } from "../domain/route";
-import { RouteId } from "../domain/routeId";
-import { RouteMap } from "../mappers/RouteMap";
+import IRouteRepo from '../services/IRepos/IRouteRepo';
+import { Route } from '../domain/route/route';
+import { RouteId } from '../domain/route/routeId';
+import { RouteMap } from '../mappers/RouteMap';
 
 import { Document, FilterQuery, Model } from 'mongoose';
 import { IRoutePersistence } from '../dataschema/IRoutePersistence';
@@ -12,62 +12,48 @@ import { IRoutePersistence } from '../dataschema/IRoutePersistence';
 export default class RouteRepo implements IRouteRepo {
   private models: any;
 
-  constructor(
-    @Inject('routeSchema') private routeSchema : Model<IRoutePersistence & Document>,
-  ) {}
+  constructor(@Inject('routeSchema') private routeSchema: Model<IRoutePersistence & Document>) {}
 
-  private createBaseQuery (): any {
+  private createBaseQuery(): any {
     return {
       where: {},
-    }
+    };
   }
 
   public async getAll(): Promise<Route[]> {
-    console.log("REPO");
-    const route =await this.routeSchema.find();
-    console.log(route);
+    const route = await this.routeSchema.find();
+
     return route.map(item => RouteMap.toDomain(item));
-
-    // try {
-    //   return this.routeSchema.find({}) as any;
-    // } catch (e) {
-    //   throw new Error(e);
-
-      
-    // }
-  
   }
 
-  public async exists(route: Route): Promise<boolean> {
-    
-    const idX = route instanceof RouteId ? (<RouteId>route).routeId : route;
+  // @ts-ignore
+  public async exists(routeId: RouteId | string): Promise<boolean> {
+    const idX = routeId instanceof RouteId ? (<RouteId>routeId).routeId : routeId;
 
-    const query = { domainId: idX}; 
-    const routeDocument = await this.routeSchema.findOne( query as FilterQuery<IRoutePersistence & Document>);
+    const query = { domainId: idX };
+    const routeDocument = await this.routeSchema.findOne(query);
 
     return !!routeDocument === true;
   }
 
-  public async save (route: Route): Promise<Route> {
-    const query = { domainId: route.routeId}; 
-
-    const routeDocument = await this.routeSchema.findOne( query );
-
+  public async save(route: Route): Promise<Route> {
+    const query = { routeId: route.routeId.routeId };
+    const routeDocument = await this.routeSchema.findOne(query);
     try {
-      if (routeDocument === null ) {
+      if (routeDocument === null) {
         const rawRoute: any = RouteMap.toPersistence(route);
 
         const routeCreated = await this.routeSchema.create(rawRoute);
 
         return RouteMap.toDomain(routeCreated);
       } else {
-        routeDocument.routeId = route.routeId; 
-        routeDocument.distance = route.distance;
-        routeDocument.routeTime = route.routeTime;
-        routeDocument.batteryWaste = route.batteryWaste;
+        routeDocument.routeId = route.routeId.routeId;
+        routeDocument.distance = route.distance.distance;
+        routeDocument.routeTime = route.routeTime.routeTime;
+        routeDocument.batteryWaste = route.batteryWaste.batteryWaste;
         routeDocument.arrivalId = route.arrivalId;
         routeDocument.departureId = route.departureId;
-        routeDocument.extraTime = route.extraTime;
+        routeDocument.extraTime = route.extraTime.extraTime;
         await routeDocument.save();
 
         return route;
@@ -77,14 +63,12 @@ export default class RouteRepo implements IRouteRepo {
     }
   }
 
-  public async findByRouteId (routeId: RouteId | string): Promise<Route> {
-    const query = { domainId: routeId};
-    const routeRecord = await this.routeSchema.findOne( query as FilterQuery<IRoutePersistence & Document> );
+  public async findByRouteId(routeId: RouteId | string): Promise<Route> {
+    const query = { routeId: routeId };
+    const routeRecord = await this.routeSchema.findOne(query as FilterQuery<IRoutePersistence & Document>);
 
-    if( routeRecord != null) {
+    if (routeRecord != null) {
       return RouteMap.toDomain(routeRecord);
-    }
-    else
-      return null;
+    } else return null;
   }
 }
--- src/services/IRepos/IRouteRepo.ts
+++ src/services/IRepos/IRouteRepo.ts
@@ -1,10 +1,9 @@
-import { Repo } from "../../core/infra/Repo";
-import { Route } from "../../domain/route";
-import { RouteId } from "../../domain/routeId";
+import { Repo } from '../../core/infra/Repo';
+import { Route } from '../../domain/route/route';
+import { RouteId } from '../../domain/route/routeId';
 
 export default interface IRouteRepo extends Repo<Route> {
-	getAll():Promise<Route[]>;
-	save(Route: Route): Promise<Route>;
-	findByRouteId (RouteId: RouteId | string): Promise<Route>;
+  getAll(): Promise<Route[]>;
+  save(route: Route): Promise<Route>;
+  findByRouteId(RouteId: RouteId | string): Promise<Route>;
 }
-  
