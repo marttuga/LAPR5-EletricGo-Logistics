540e806ff9454ca35956574eb1989d070542fbe7
João Tiago Araújo <1200584@isep.ipp.pt>
2022-11-04 18:03:42 +0000

    updated route controller and persistence

--- src/controllers/routeController.ts
+++ src/controllers/routeController.ts
@@ -1,82 +1,77 @@
 import { Request, Response, NextFunction } from 'express';
 import { Inject, Service } from 'typedi';
-import config from "../../config";
+import config from '../../config';
 
-import IRouteController from "./IControllers/IRouteController";
+import IRouteController from './IControllers/IRouteController';
 import IRouteService from '../services/IServices/IRouteService';
 import IRouteDTO from '../dto/IRouteDTO';
 
-import { Result } from "../core/logic/Result";
+import { Result } from '../core/logic/Result';
 
 @Service()
 export default class RouteController implements IRouteController /* TODO: extends ../core/infra/BaseController */ {
-  constructor(
-      @Inject(config.services.route.name) private routeServiceInstance : IRouteService
-  ) {}
+  constructor(@Inject(config.services.route.name) private routeServiceInstance: IRouteService) {}
 
   public async getRoute(req: Request, res: Response, next: NextFunction) {
     try {
-      console.log("CONTROLLER");
-      //const routeOrError = await this.routeServiceInstance.getRoutes() as Result<IRouteDTO>;
       const routeOrError = await this.routeServiceInstance.getRoutes();
-
 
       if (routeOrError.isFailure) {
         return res.status(404).send();
       }
 
       const routeDTO = routeOrError.getValue();
-      return res.json( routeDTO ).status(201);    }
-    catch (e) {
+      return res.json(routeDTO).status(201);
+    } catch (e) {
       return next(e);
     }
-  };
+  }
 
   public async createRoute(req: Request, res: Response, next: NextFunction) {
     try {
-      const RouteOrError = await this.routeServiceInstance.createRoute(req.body as IRouteDTO) as Result<IRouteDTO>;
-        
+      const RouteOrError = (await this.routeServiceInstance.createRoute(
+        req.params.routeId as string,
+        req.body as IRouteDTO,
+      )) as Result<IRouteDTO>;
+
       if (RouteOrError.isFailure) {
         return res.status(402).send();
       }
 
       const RouteDTO = RouteOrError.getValue();
-      return res.json( RouteDTO ).status(201);
-    }
-    catch (e) {
+      return res.json(RouteDTO).status(201);
+    } catch (e) {
       return next(e);
     }
-  };
+  }
 
   public async updateRoute(req: Request, res: Response, next: NextFunction) {
     try {
-      const routeOrError = await this.routeServiceInstance.updateRoute(req.body as IRouteDTO) as Result<IRouteDTO>;
+      const routeOrError = (await this.routeServiceInstance.updateRoute(req.body as IRouteDTO)) as Result<IRouteDTO>;
 
       if (routeOrError.isFailure) {
         return res.status(404).send();
       }
 
       const routeDTO = routeOrError.getValue();
-      return res.json( routeDTO ).status(201);
-    }
-    catch (e) {
+      return res.json(routeDTO).status(201);
+    } catch (e) {
       return next(e);
     }
-  };
+  }
 
   public async getRouteId(req: Request, res: Response, next: NextFunction) {
     try {
-      const RouteOrError = await this.routeServiceInstance.getRouteId(req.body as string) as Result<IRouteDTO>;
+      const RouteOrError = (await this.routeServiceInstance.getRouteId(req.body as string)) as Result<IRouteDTO>;
 
       if (RouteOrError.isFailure) {
         return res.status(404).send();
       }
 
       const routeDTO = RouteOrError.getValue();
-      return res.json( routeDTO ).status(201);
-    }
-    catch (e) {
+      return res.json(routeDTO).status(201);
+    } catch (e) {
       return next(e);
     }
-  };
+  }
 }
--- src/dataschema/IRoutePersistence.ts
+++ src/dataschema/IRoutePersistence.ts
@@ -1,16 +1,16 @@
-import { BatteryWaste } from "../domain/batteryWaste";
-import { Distance } from "../domain/distance";
-import { ExtraTime } from "../domain/extraTime";
-import { RouteId } from "../domain/routeId";
-import { RouteTime } from "../domain/routeTime";
+import { BatteryWaste } from '../domain/route/batteryWaste';
+import { Distance } from '../domain/route/distance';
+import { ExtraTime } from '../domain/route/extraTime';
+import { RouteId } from '../domain/route/routeId';
+import { RouteTime } from '../domain/route/routeTime';
 
 export interface IRoutePersistence {
-    _id: string;
-    routeId: RouteId;
-    distance: Distance;
-    routeTime: RouteTime;
-    batteryWaste: BatteryWaste;
-    arrivalId: string;
-    departureId: string;
-    extraTime: ExtraTime;
-  }
+  _id: string;
+  routeId: string;
+  distance: string;
+  routeTime: string;
+  batteryWaste: string;
+  arrivalId: string;
+  departureId: string;
+  extraTime: string;
+}
