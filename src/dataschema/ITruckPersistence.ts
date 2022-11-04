2bd99e1949489852595ff4420ac25b70d231ee51
Marta Portugal <1201845@isep.ipp.pt>
2022-11-04 21:17:58 +0000

    truck persistence and dto alteration

--- src/dataschema/ITruckPersistence.ts
+++ src/dataschema/ITruckPersistence.ts
@@ -1,16 +1,23 @@
-import { AutonomyFullChargeLoad } from "../domain/autonomyFullChargeLoad";
-import { Capacity } from "../domain/capacity";
-import { LicencePlate } from "../domain/licencePlate";
-import { MaxBateryCapacity } from "../domain/maxBateryCapacity";
-import { Tare } from "../domain/tare";
-import { TimeCharging } from "../domain/timeCharging";
+import { AutonomyFullChargeLoad } from "../domain/truck/autonomyFullChargeLoad";
+import { Capacity } from "../domain/truck/capacity";
+import { LicencePlate } from "../domain/truck/licencePlate";
+import { MaxBateryCapacity } from "../domain/truck/maxBateryCapacity";
+import { Tare } from "../domain/truck/tare";
+import { TimeCharging } from "../domain/truck/timeCharging";
 
 export interface ITruckPersistence {
 	_id: string;
+	licencePlate:string;
+	tare: number;
+	capacity:number;
+	maxBateryCapacity:number;
+	autonomyFullChargeLoad:number;
+	timeCharging: number;
+/* 	_id: string;
 	licencePlate:LicencePlate;
 	tare: Tare;
 	capacity:Capacity;
 	maxBateryCapacity:MaxBateryCapacity;
 	autonomyFullChargeLoad:AutonomyFullChargeLoad;
-	timeCharging: TimeCharging;
+	timeCharging: TimeCharging; */
   }
