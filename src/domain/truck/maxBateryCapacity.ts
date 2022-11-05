75f146cc60585c08e9a4ea0833eec33d35852665
Marta Portugal <1201845@isep.ipp.pt>
2022-11-05 00:56:41 +0000

    valueObject correction

--- src/domain/truck/maxBateryCapacity.ts
+++ src/domain/truck/maxBateryCapacity.ts
@@ -1,27 +1,26 @@
-import { ValueObject } from '../../core/domain/ValueObject';
-import { Guard } from '../../core/logic/Guard';
-import { Result } from '../../core/logic/Result';
+import { ValueObject } from "../../core/domain/ValueObject";
+import { Guard } from "../../core/logic/Guard";
+import { Result } from "../../core/logic/Result";
 
 interface MaxBateryCapacityProps {
-  maxBateryCapacity: number;
+  value: number;
 }
 
 export class MaxBateryCapacity extends ValueObject<MaxBateryCapacityProps> {
-  get maxBateryCapacity(): number {
-    return this.props.maxBateryCapacity;
+  get value (): number {
+    return this.props.value;
   }
-
-  private constructor(props: MaxBateryCapacityProps) {
+  
+  private constructor (props: MaxBateryCapacityProps) {
     super(props);
   }
 
-  public static create(props: MaxBateryCapacityProps): Result<MaxBateryCapacity> {
-    const guardResult = Guard.againstNullOrUndefined(props, 'maxBateryCapacity');
-
+   public static create (maxBateryCapacity: number): Result<MaxBateryCapacity> {
+    const guardResult = Guard.againstNullOrUndefined(maxBateryCapacity, 'maxBateryCapacity');
     if (!guardResult.succeeded) {
       return Result.fail<MaxBateryCapacity>(guardResult.message);
     } else {
-      return Result.ok<MaxBateryCapacity>(new MaxBateryCapacity({ maxBateryCapacity: props.maxBateryCapacity }));
+      return Result.ok<MaxBateryCapacity>(new MaxBateryCapacity({ value: maxBateryCapacity }))
     }
-  }
+  } 
 }
