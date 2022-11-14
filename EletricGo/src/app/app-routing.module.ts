e71a71a86571af38e71aff2ceee67d47b6e821ca
Marta Portugal <1201845@isep.ipp.pt>
2022-11-14 16:26:40 +0200

    app module changes

--- EletricGo/src/app/app-routing.module.ts
+++ EletricGo/src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateTruckComponent } from './views/create-truck/create-truck.component';
+import { FleetManagerComponent } from './views/fleet-manager/fleet-manager.component';
 import {LoginComponent} from "./views/login/login.component";
 import {NetworkComponent} from "./views/network/network.component";
 
@@ -7,6 +9,9 @@ const routes: Routes = [
   { path: '',redirectTo: '/views/login', pathMatch: 'full' },
   { path: 'views/login', component: LoginComponent},
   { path: 'views/network', component:NetworkComponent},
+  { path: 'views/create-truck', component:CreateTruckComponent},
+  { path: 'views/fleet-manager', component:FleetManagerComponent},
+
 
 ];
 
--- EletricGo/src/app/app.module.ts
+++ EletricGo/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './views/login/login.component';
 import { NetworkComponent } from './views/network/network.component';
 import { FleetManagerComponent } from './views/fleet-manager/fleet-manager.component';
+import { CreateTruckComponent } from './views/create-truck/create-truck.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     NetworkComponent,
-    FleetManagerComponent
+    FleetManagerComponent,
+    CreateTruckComponent
   ],
   imports: [
     BrowserModule,
