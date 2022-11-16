299038b37c7e5e2e00b2773bb239704b2b377497
Marta Portugal <1201845@isep.ipp.pt>
2022-11-16 09:45:05 +0200

    LEI2223-27 create truck component

--- EletricGo/src/app/views/create-truck/create-truck.component.ts
+++ EletricGo/src/app/views/create-truck/create-truck.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {TrucksService} from "../../services/node/truck.service";
+  import { Component, OnInit } from '@angular/core';
+  import {ActivatedRoute, Router} from "@angular/router";
+  import {Observable} from "rxjs";
+
 
 @Component({
-  selector: 'app-create-truck',
+  selector: 'app-fleet-manager',
   templateUrl: './create-truck.component.html',
   styleUrls: ['./create-truck.component.css']
 })
-export class CreateTruckComponent implements OnInit {
+export class FleetManagerComponent implements OnInit {
 
-  constructor() { }
+ licencePlate:string;
+ tare:number;
+ capacity:number;
+ maxBateryCapacity:number;
+ autonomyFullChargeLoad:number;
+ timeCharging:number;
+
+
+  constructor( private truckService: TrucksService,
+               private route: ActivatedRoute,
+               private router: Router) {
+
+  }
 
   ngOnInit(): void {
+  }
+
+  public ():void{
+    this.truckService.createTruck(this.licencePlate,this.tare,this.capacity,this.maxBateryCapacity,this.autonomyFullChargeLoad,this.timeCharging).subscribe(data => {console.log(data)});
+
   }
 
 }
