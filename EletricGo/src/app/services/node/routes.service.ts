64d62d21e54426b86ac4800bc9ba8ecdd407acc6
João Tiago Araújo <1200584@isep.ipp.pt>
2022-11-20 17:41:26 +0000

    updated routeServices

--- EletricGo/src/app/services/node/routes.service.ts
+++ EletricGo/src/app/services/node/routes.service.ts
@@ -6,36 +6,44 @@ import {HttpClient} from "@angular/common/http";
   providedIn: 'root'
 })
 export class RoutesService {
-  private Url = 'http://localhost:3000/api/routes/';
+  private Url = 'http://localhost:3000/api/routes';
 
   constructor(private httpClient: HttpClient) { }
 
-  createRoute(routeId:string,distance:string,routeTime:string,batteryWaste:string,arrivalId:string,departureId:string,extraTime:string): Observable<any> {
-    const body={
-      "RouteId": {
-        "RouteId": routeId
-      },
-      "Distance": {
-        "Distance": distance
-      },
-      "RouteTime": {
-        "RouteTime": routeTime
-      },
-      "BatteryWaste": {
-        "BatteryWaste": batteryWaste
-      },
-      "ArrivalId": {
-        "ArrivalId": arrivalId
-      },
-      "DepartureId": {
-        "DepartureId": departureId
-      },
-      "ExtraTime": {
-        "ExtraTime": extraTime
-      }
-    }
-    ;
-    return this.httpClient.post(this.Url ,body).pipe(map(this.extractData));
+  // createRoute(routeId:string,distance:string,routeTime:string,batteryWaste:string,arrivalId:string,departureId:string,extraTime:string): Observable<any> {
+  //   const body={
+  //     "RouteId": {
+  //       "RouteId": routeId
+  //     },
+  //     "Distance": {
+  //       "Distance": distance
+  //     },
+  //     "RouteTime": {
+  //       "RouteTime": routeTime
+  //     },
+  //     "BatteryWaste": {
+  //       "BatteryWaste": batteryWaste
+  //     },
+
+  //     "ArrivalId": arrivalId,
+
+  //     "DepartureId": departureId,
+
+  //     "ExtraTime": {
+  //       "ExtraTime": extraTime
+  //     }
+  //   }
+  //   ;
+  //   return this.httpClient.post(this.Url ,body).pipe(map(this.extractData));
+  // }
+
+  createRoute(routeId: string,distance:string,routeTime:string,batteryWaste:string,arrivalId: string, departureId: string, extraTime:string) {
+    const body={"routeId":routeId, "distance":distance, "routeTime":routeTime, "batteryWaste":batteryWaste, "arrivalId": arrivalId, "departureId":departureId,"extraTime":extraTime};
+    console.log(body);
+    return this.httpClient.post(this.Url + '/createRoute',body)
+      .pipe(map(this.extractData)
+      );
+
   }
 
   public getRoutes(): Observable<any> {
