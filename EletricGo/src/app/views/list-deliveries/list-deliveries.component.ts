4584e4b061217d69c0857bada63dbfca660fb284
Maria Flores <1201615@isep.ipp.pt>
2022-11-21 17:28:30 +0000

    <LEI2223-26>  create list deliveries component

---
+++ EletricGo/src/app/views/list-deliveries/list-deliveries.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {DeliveriesService} from "../../services/dotnet/deliveries.service";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-list-deliveries',
+  templateUrl: './list-deliveries.component.html',
+  styleUrls: ['./list-deliveries.component.css']
+})
+export class ListDeliveriesComponent implements OnInit {
+
+  deliveries: Delivery[];
+  delivery:Delivery;
+  dIdentifier:string;
+ date:number;
+ mass:number;
+ timeLoad:number;
+ timeUnload:number;
+  constructor(private deliveryService: DeliveriesService,
+              private route: ActivatedRoute,
+              private router: Router) { }
+
+  ngOnInit(): void {
+    this.listDeliveries();
+  }
+
+  public listDeliveries():void{
+    this.deliveryService.getDeliveries().subscribe(data => {console.log(data);
+      this.deliveries=data});
+  }
+
+}
