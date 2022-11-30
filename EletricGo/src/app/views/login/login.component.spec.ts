6eceabf70296d6a86fe3e51baff78cc6dccc97db
Marta Portugal <1201845@isep.ipp.pt>
2022-11-30 02:32:13 +0000

    Unit testing correction for login and network page

--- EletricGo/src/app/views/login/login.component.spec.ts
+++ EletricGo/src/app/views/login/login.component.spec.ts
@@ -1,5 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -9,7 +11,7 @@ describe('LoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ LoginComponent ],
-      imports:[FormsModule]
+      imports:[HttpClientTestingModule,FormsModule,RouterTestingModule],
     })
     .compileComponents();
 
