e1e341fc9e711a28d1cbc8a614121ea943ad2200
Marta Portugal <1201845@isep.ipp.pt>
2022-11-30 02:29:17 +0000

    LEI2223-67 unit testing for truck correction

--- EletricGo/src/app/views/list-truck/list-truck.component.spec.ts
+++ EletricGo/src/app/views/list-truck/list-truck.component.spec.ts
@@ -1,6 +1,9 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ListTruckComponent } from './list-truck.component';
+import { FilterPipe } from "../model/filterPipe";
 
 describe('ListTruckComponent', () => {
   let component: ListTruckComponent;
@@ -8,8 +11,8 @@ describe('ListTruckComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ListTruckComponent ],
-      imports:[FormsModule]
+      declarations: [FilterPipe, ListTruckComponent ],
+      imports:[HttpClientTestingModule,FormsModule,RouterTestingModule],
     })
     .compileComponents();
 
