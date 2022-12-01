af7c3e52ed151ee14205929bb9fbdf4b290d5700
Maria Flores <1201615@isep.ipp.pt>
2022-12-01 19:38:14 +0000

    <LEI2223-52> unit testing for list of deliveries

--- EletricGo/src/app/services/dotnet/deliveries.service.ts
+++ EletricGo/src/app/services/dotnet/deliveries.service.ts
@@ -6,7 +6,7 @@ import {map, Observable} from "rxjs";
   providedIn: 'root'
 })
 export class DeliveriesService {
-  private Url = 'https://localhost:5001/api/Deliveries';
+   Url = 'https://localhost:5001/api/Deliveries';
 
   constructor(private httpClient: HttpClient) { }
 
@@ -33,6 +33,19 @@ export class DeliveriesService {
 
   public extractData(res: any) {
     return res || { };
+  }
+
+  updateDelivery(dIdentifier:string,date:number,mass:number,timeLoad:number,timeUnload:number,deliveryWarehouse:string){
+    const body={ 
+    "Date": date,
+    "Mass": mass,
+    "TimeLoad": timeLoad,
+    "TimeUnload": timeUnload,
+    "DeliveryWarehouse":deliveryWarehouse,};
+  console.log(body);
+    return this.httpClient.put(this.Url + '/Update' + dIdentifier,body)
+      .pipe(map(this.extractData)
+      );
   }
 }
 
