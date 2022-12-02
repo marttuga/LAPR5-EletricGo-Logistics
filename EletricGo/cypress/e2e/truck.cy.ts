2399a03a2198fe00fcc006eff6bda87a85f9d999
Marta Portugal <1201845@isep.ipp.pt>
2022-12-02 17:21:12 +0000

    truck component altrations

--- EletricGo/cypress/e2e/truck.cy.ts
+++ EletricGo/cypress/e2e/truck.cy.ts
@@ -3,7 +3,7 @@ describe('Truck Test', () => {
     beforeEach(() => {
       // run these tests as if in a desktop
       // browser with a 720p monitor
-      cy.viewport(1800, 900)
+      cy.viewport(1500, 800)
     })
 
     it('Initial project page', () => {
@@ -42,16 +42,14 @@ describe('Truck Test', () => {
 
     it('creating truck fails', () => {
       cy.get('[class="textfield"]').should('have.length',6)
-      cy.get('[type="text"]').type('FFbkkf')
-      cy.get('[type="number"]').eq(0).type('12000')
-      cy.get('[type="number"]').eq(1).type('15000')
-      cy.get('[type="number"]').eq(2).type('120')
-      cy.get('[type="number"]').eq(3).type('120')
-      cy.get('[type="number"]').eq(4).type('2')
+      cy.get('[type="text"]').type('FFF')
+      cy.get('[type="number"]').eq(0).type('8')
+      cy.get('[type="number"]').eq(1).type('10')
+      cy.get('[type="number"]').eq(2).type('12')
+      cy.get('[type="number"]').eq(3).type('12')
+      cy.get('[type="number"]').eq(4).type('0')
 
-      cy.get('[type="submit"]').click()
-
-      cy.get('[type="button"]').click()
+      cy.get('[type="submit"]').should('be.disabled')
 
       cy.get('[type="text"]').clear()
       cy.get('[type="number"]').eq(0).clear()
@@ -63,36 +61,56 @@ describe('Truck Test', () => {
 
     })
 
+
     it('Visits the list Truck page', () => {
       cy.visit('/views/list-truck')
       cy.get('h2').contains('ALL EXISTENT TRUCKS')
     })
 
     it('Searches Truck', () => {
-      cy.get('[class="table"]').should('have.length',6)
-      cy.get('[type="text"]').type('FF-12-Ff')
-      cy.get('[class="table"]').contains('FF-12-Ff')
+      cy.get('th').should('have.length',7)
+      cy.get('[type="text"]').type('FFF')
     })
 
-
-
-/*     it('creates truck', () => {
+    it('Visits the create Truck page', () => {
       cy.visit('/views/create-truck')
+      cy.get('h2').contains('NEED MORE TRUCKS?')
+      cy.get('h3').contains('CREATE ANOTHER ONE')
+    })
+
+    it('creating truck success', () => {
       cy.get('[class="textfield"]').should('have.length',6)
-      cy.get('[type="text"]').type('FF-12-Ff')
+      cy.get('[type="text"]').type('AA-11-AA')
       cy.get('[type="number"]').eq(0).type('12000')
       cy.get('[type="number"]').eq(1).type('15000')
       cy.get('[type="number"]').eq(2).type('120')
       cy.get('[type="number"]').eq(3).type('120')
-      cy.get('[type="number"]').eq(4).type('2')
+      cy.get('[type="number"]').eq(4).type('4')
+
       cy.get('[type="submit"]').click()
+
+      cy.get('[type="text"]').clear()
+      cy.get('[type="number"]').eq(0).clear()
+      cy.get('[type="number"]').eq(1).clear()
+      cy.get('[type="number"]').eq(2).clear()
+      cy.get('[type="number"]').eq(3).clear()
+      cy.get('[type="number"]').eq(4).clear()
+     
+
     })
-  
+
+
     it('Visits the list Truck page', () => {
       cy.visit('/views/list-truck')
       cy.get('h2').contains('ALL EXISTENT TRUCKS')
-   
     })
- */
+
+    it('Searches Truck', () => {
+      cy.get('th').should('have.length',7)
+      cy.get('[type="text"]').type('AA-11-AA')
+      cy.get('[class="table"]').contains('AA-11-AA')
+
+
+    })
 
   })
