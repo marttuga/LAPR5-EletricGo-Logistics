fecf8ef45275425f23206e60951c7e865b43df38
Maria Flores <1201615@isep.ipp.pt>
2022-12-02 20:20:57 +0000

    <LEI2223-52> tests end2end Deliveries

--- EletricGo/cypress/e2e/delivery.cy.ts
+++ EletricGo/cypress/e2e/delivery.cy.ts
@@ -1,4 +1,4 @@
-/*describe('Delivery Test', () => {
+describe('Delivery Test', () => {
  
     beforeEach(() => {
       cy.viewport(1800, 900)
@@ -26,8 +26,6 @@
     it('Visits the warehouse manager page', () => {
       cy.get('[class="btn-login"]').contains('Warehouse Manager').click()
       cy.visit('/views/warehouse-manager')
-      cy.get('h1').contains('Welcome back!')
-      cy.get('h2').contains(" Let's get to work!😉")
       cy.contains('Create Delivery').click()
     })
 
@@ -38,13 +36,13 @@
       cy.get('h2').contains('Enter all information here!')
     })
 
-    it('creating delivery fails', () => {
-      cy.get('[type="text"]').type('D01')
-      cy.get('[type="text"]').eq(0).type('12052022')
-      cy.get('[type="number"]').eq(1).type('15')
-      cy.get('[type="number"]').eq(2).type('2')
-      cy.get('[type="number"]').eq(3).type('7')
-      cy.get('[type="text"]').eq(4).type('W01')
+    /*it('creating delivery fails', () => {
+      cy.get('[type="text"]').type('01')
+      cy.get('[type="text"]').eq(0).type('2222')
+      cy.get('[type="number"]').eq(1).type('-5')
+      cy.get('[type="number"]').eq(2).type('0')
+      cy.get('[type="number"]').eq(3).type('0')
+      cy.get('[type="text"]').eq(4).type('2')
 
       cy.get('[type="submit"]').click()
 
@@ -58,13 +56,46 @@
       cy.get('[type="text"]').eq(4).clear()
      
 
-    })
+    })*/
 
     it('Visits the list Delivery page', () => {
       cy.visit('/views/list-deliveries')
       cy.get('h2').contains('All Deliveries')
     })
 
+    it('Visits the create Delivery page', () => {
+      cy.visit('/views/create-delivery')
+      cy.get('h1').contains('New Delivery?')
+      cy.get('h2').contains('Enter all information here!')
+    })
+/*
+    it('creating delivery success', () => {
+      cy.get('[type="text"]').type('D01')
+      cy.get('[type="text"]').eq(0).type('12052022')
+      cy.get('[type="number"]').eq(1).type('15')
+      cy.get('[type="number"]').eq(2).type('2')
+      cy.get('[type="number"]').eq(3).type('7')
+      cy.get('[type="text"]').eq(4).type('W01')
 
+      cy.get('[type="submit"]').click()
 
-  })*/
+      
+
+      cy.get('[type="text"]').clear()
+      cy.get('[type="text"]').eq(0).clear()
+      cy.get('[type="number"]').eq(1).clear()
+      cy.get('[type="number"]').eq(2).clear()
+      cy.get('[type="number"]').eq(3).clear()
+      cy.get('[type="text"]').eq(4).clear()
+     
+
+    })*/
+
+    it('Visits the list Deliveries page', () => {
+      cy.visit('/views/list-deliveries')
+      cy.get('h2').contains('All Deliveries')
+    })
+
+    
+
+  })
