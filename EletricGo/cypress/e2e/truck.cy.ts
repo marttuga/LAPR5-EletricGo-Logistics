add07874746d656a7b8166b64d8f37de8d590a10
Marta Portugal <1201845@isep.ipp.pt>
2022-12-02 22:57:07 +0000

    LEI2223-27 e2e tests

--- EletricGo/cypress/e2e/truck.cy.ts
+++ EletricGo/cypress/e2e/truck.cy.ts
@@ -15,7 +15,7 @@ describe('Truck Test', () => {
 
     it('Login page', () => {
       cy.visit('/views/login')
-      cy.get('h1').contains('LOGIN')
+      cy.get('h1').should('contain','LOGIN')
     })
 
     it('user and password shoul be empty, with buttons bellow', () => {
@@ -26,89 +26,111 @@ describe('Truck Test', () => {
     })
 
     it('Visits the fleet manager page', () => {
+      cy.get('[class="btn-login"]').should('contain','Fleet Manager')
       cy.get('[class="btn-login"]').contains('Fleet Manager').click()
       cy.visit('/views/fleet-manager')
-      cy.get('p').contains('WELCOME')
-      cy.get('h1').contains('FLEET MANAGER!')
-      cy.contains('Manage Trucks').click()
+      cy.get('p').should('contain','WELCOME')
+      cy.get('h1').should('contain','FLEET MANAGER!')
+      cy.get('[class="navbar"]').should('contain','Home')
+      cy.get('[class="navbar"]').should('contain','Manage Trucks')
+      cy.get('[class="navbar"]').should('contain','See all Trucks')
     })
 
 
     it('Visits the create Truck page', () => {
       cy.visit('/views/create-truck')
-      cy.get('h2').contains('NEED MORE TRUCKS?')
-      cy.get('h3').contains('CREATE ANOTHER ONE')
+      cy.get('h2').should('contain','NEED MORE TRUCKS?')
+      cy.get('h3').should('contain','CREATE ANOTHER ONE')
     })
 
     it('creating truck fails', () => {
       cy.get('[class="textfield"]').should('have.length',6)
       cy.get('[type="text"]').type('FFF')
+      
       cy.get('[type="number"]').eq(0).type('8')
+      cy.get('[class="requiredError"]').should('be.visible').should('contain','Licence Plate must must be of format XX-00-XX!')
+      
       cy.get('[type="number"]').eq(1).type('10')
+      cy.get('[class="requiredError"]').should('be.visible').should('contain','Tare must be between 3000kg and 20000kg!')
+     
       cy.get('[type="number"]').eq(2).type('12')
+      cy.get('[class="requiredError"]').should('be.visible').should('contain','Capacity must be between 3000kg and 20000kg!')
+     
       cy.get('[type="number"]').eq(3).type('12')
+       cy.get('[class="requiredError"]').should('be.visible').should('contain','Max batery capacity must be between 80kWh and 200kWh!')
+     
       cy.get('[type="number"]').eq(4).type('0')
+      cy.get('[class="requiredError"]').should('be.visible').should('contain','Autonomy must be between 100km and 8000km!')
+
 
       cy.get('[type="submit"]').should('be.disabled')
+    
 
-      cy.get('[type="text"]').clear()
+      cy.get('[type="text"]').clear()   
+      cy.get('[class="requiredError"]').should('be.visible').should('contain','Time to charge must be between 1h and 5h!')
+      cy.get('[class="requiredError"]').should('be.visible').should('contain','Licence Plate is required!')
+
       cy.get('[type="number"]').eq(0).clear()
+      cy.get('[class="requiredError"]').should('be.visible').should('contain','Tare is required!')
+
       cy.get('[type="number"]').eq(1).clear()
+      cy.get('[class="requiredError"]').should('be.visible').should('contain','Capacity is required!')
+
       cy.get('[type="number"]').eq(2).clear()
+      cy.get('[class="requiredError"]').should('be.visible').should('contain','Max batery capacity is required!')
+
       cy.get('[type="number"]').eq(3).clear()
+      cy.get('[class="requiredError"]').should('be.visible').should('contain','Autonomy is required!')
+
       cy.get('[type="number"]').eq(4).clear()
-     
+      cy.get('[class="requiredError"]').should('be.visible').should('contain','Time is required!')
 
     })
 
 
     it('Visits the list Truck page', () => {
       cy.visit('/views/list-truck')
-      cy.get('h2').contains('ALL EXISTENT TRUCKS')
+      cy.get('h2').should('contain','ALL EXISTENT TRUCKS')
     })
 
-    it('Searches Truck', () => {
+    it('Searches Truck failed', () => {
       cy.get('th').should('have.length',7)
       cy.get('[type="text"]').type('FFF')
     })
 
     it('Visits the create Truck page', () => {
       cy.visit('/views/create-truck')
-      cy.get('h2').contains('NEED MORE TRUCKS?')
-      cy.get('h3').contains('CREATE ANOTHER ONE')
+      cy.get('h2').should('contain','NEED MORE TRUCKS?')
+      cy.get('h3').should('contain','CREATE ANOTHER ONE')
     })
 
     it('creating truck success', () => {
       cy.get('[class="textfield"]').should('have.length',6)
       cy.get('[type="text"]').type('AA-11-AA')
+
+
       cy.get('[type="number"]').eq(0).type('12000')
       cy.get('[type="number"]').eq(1).type('15000')
       cy.get('[type="number"]').eq(2).type('120')
       cy.get('[type="number"]').eq(3).type('120')
       cy.get('[type="number"]').eq(4).type('4')
 
+      cy.get('[type="submit"]').should('be.enabled')
       cy.get('[type="submit"]').click()
-
-      cy.get('[type="text"]').clear()
-      cy.get('[type="number"]').eq(0).clear()
-      cy.get('[type="number"]').eq(1).clear()
-      cy.get('[type="number"]').eq(2).clear()
-      cy.get('[type="number"]').eq(3).clear()
-      cy.get('[type="number"]').eq(4).clear()
-     
+      cy.get('[class="created-message"]').should('be.visible').contains("Truck created!")
 
     })
 
 
     it('Visits the list Truck page', () => {
       cy.visit('/views/list-truck')
-      cy.get('h2').contains('ALL EXISTENT TRUCKS')
+      cy.get('h2').should('contain','ALL EXISTENT TRUCKS')
     })
 
-    it('Searches Truck', () => {
+    it('Searches Truck success', () => {
       cy.get('th').should('have.length',7)
       cy.get('[type="text"]').type('AA-11-AA')
-      cy.get('[class="table"]').contains('AA-11-AA')
+      cy.get('[class="table"]').should('contain','AA-11-AA')
 
 
     })
--- EletricGo/src/app/views/create-truck/create-truck.component.spec.ts
+++ EletricGo/src/app/views/create-truck/create-truck.component.spec.ts
@@ -5,13 +5,15 @@ import { CreateTruckComponent } from './create-truck.component';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from "@angular/router/testing";
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 
 describe('CreateTruckComponent', () => {
   let service: TrucksService;
   let component: CreateTruckComponent;
   let fixture: ComponentFixture<CreateTruckComponent>;
-
+  let debugElement: DebugElement;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports:[HttpClientTestingModule,FormsModule,RouterTestingModule],
@@ -22,6 +24,7 @@ describe('CreateTruckComponent', () => {
     fixture = TestBed.createComponent(CreateTruckComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    
   });
 
   it('component should be created',() => {
@@ -45,7 +48,16 @@ describe('CreateTruckComponent', () => {
     expect(compiled.querySelector('h3')?.textContent).toContain('CREATE ANOTHER ONE');
   });
 
-
+/*   it('should hide contents if show is false', () => {
+    // should be rendered initially
+    expect(debugElement.query(By.css('.textfield')).nativeElement).toBeTruthy();
+    //trigger change
+    const button = debugElement.query(By.css('button')).nativeElement;
+    button.click();   // this will change show to false
+    fixture.detectChanges();
+    // should not be rendered
+    expect(debugElement.query(By.css('.textfield')).nativeElement).toBeFalsy();
+}); */
 
 
     
