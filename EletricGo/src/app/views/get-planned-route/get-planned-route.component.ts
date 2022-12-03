928f50703a51f102261228db78894a3cd67c196e
João Tiago Araújo <1200584@isep.ipp.pt>
2022-12-03 21:40:51 +0000

    updated get-planned-route components

--- EletricGo/src/app/views/get-planned-route/get-planned-route.component.ts
+++ EletricGo/src/app/views/get-planned-route/get-planned-route.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-get-planned-route',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GetPlannedRouteComponent implements OnInit {
 
-  constructor() { }
+  plannedRoutes: PlannedRoute[];
+  truckId: Truck;
+  arrivalId: string;
+  departureId: string;
+  date: string;
+  totalTime: string;
+  searchDate: string;
+
+  constructor() {
+
+}
 
   ngOnInit(): void {
+    //this.getPlannedRoutes();
   }
 
+  // public getPlannedRoutes():void{
+  //   //this.routeService.getRoutes().subscribe(data => {console.log(data);
+  //   this.plannedRoutes=data});
+  //   }
 }
