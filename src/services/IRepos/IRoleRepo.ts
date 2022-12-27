1b0758e4dd00acca412acff2096a3c95ac2591d1
Marta Portugal <1201845@isep.ipp.pt>
2022-12-27 22:13:13 +0000

    correction

--- src/services/IRepos/IRoleRepo.ts
+++ src/services/IRepos/IRoleRepo.ts
@@ -3,7 +3,6 @@ import { Role } from "../../domain/role/role";
 import { RoleName } from "../../domain/role/roleName";
 
 export default interface IRoleRepo extends Repo<Role> {
-  exists(roleName: RoleName | string): Promise<boolean>
   getAllRoles(): Promise<Role[]>
   save(role: Role): Promise<Role>
   findByName (name: RoleName | string): Promise<Role>
