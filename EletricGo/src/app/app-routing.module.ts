d68778ea9bd4d6459092538c699cbe5a63f1715e
João Tiago Araújo <1200584@isep.ipp.pt>
2023-01-04 10:28:47 +0000

    updated routing at app modules

--- EletricGo/src/app/app-routing.module.ts
+++ EletricGo/src/app/app-routing.module.ts
@@ -3,30 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { CreateTruckComponent } from './views/create-truck/create-truck.component';
 import { FleetManagerComponent } from './views/fleet-manager/fleet-manager.component';
 import {LoginComponent} from "./views/login/login.component";
-import {NetworkComponent} from "./views/network/network.component";
-import {LogisticsManagerComponent} from "./views/logistics-manager/logistics-manager.component";
-import {CreateRouteComponent} from "./views/create-route/create-route.component";
 import {ListTruckComponent} from "./views/list-truck/list-truck.component";
-import { GetPlannedRouteComponent } from './views/get-planned-route/get-planned-route.component';
-import { ListRoutesComponent } from './views/list-routes/list-routes.component';
-import { ListPlannedRoutesComponent } from './views/list-planned-routes/list-planned-routes.component';
 import { CreateUserComponent } from './views/create-user/create-user.component';
 import { AdminPageComponent } from './views/admin-page/admin-page.component';
+import { NetworkComponent } from './views/network/network.component';
 
 
 const routes: Routes = [
   { path: '',redirectTo: '/views/login', pathMatch: 'full' },
   { path: 'views/login', component: LoginComponent},
-  { path: 'views/network', component:NetworkComponent},
-  { path: 'views/logistics-manager', component:LogisticsManagerComponent},
+  { path: 'views/network', component: NetworkComponent},
   { path: 'views/fleet-manager', component:FleetManagerComponent},
   { path: 'views/create-truck', component:CreateTruckComponent},
-  { path: 'views/create-route', component:CreateRouteComponent},
   { path: 'views/create-truck', component:CreateTruckComponent},
   { path: 'views/list-truck', component:ListTruckComponent},
-  { path: 'views/get-planned-route', component:GetPlannedRouteComponent},
-  { path: 'views/list-routes', component:ListRoutesComponent},
-  { path: 'views/list-planned-routes', component:ListPlannedRoutesComponent},
   { path: 'views/create-user', component:CreateUserComponent},
   { path: 'views/admin-page', component:AdminPageComponent},
 
--- EletricGo/src/app/app.module.ts
+++ EletricGo/src/app/app.module.ts
@@ -5,23 +5,22 @@ import { NgxPaginationModule } from 'ngx-pagination';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './views/login/login.component';
-import { NetworkComponent } from './views/network/network.component';
 import {FormsModule} from "@angular/forms";
 import { FleetManagerComponent } from './views/fleet-manager/fleet-manager.component';
 import { CreateTruckComponent } from './views/create-truck/create-truck.component';
 import {HttpClientModule} from "@angular/common/http";
-import { CreateRouteComponent } from './views/create-route/create-route.component';
-import { LogisticsManagerComponent } from './views/logistics-manager/logistics-manager.component';
 import { ListTruckComponent } from './views/list-truck/list-truck.component';
-import { GetPlannedRouteComponent } from './views/get-planned-route/get-planned-route.component';
-import { ListRoutesComponent } from './views/list-routes/list-routes.component';
 import { SharedModule } from './views/shared.module';
-import { ListPlannedRoutesComponent } from './views/list-planned-routes/list-planned-routes.component';
 import { TruckStatusComponent } from './views/truck-status/truck-status.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {WarehouseManagerModule} from "./views/warehouse-manager/warehouse-manager.module";
 import { CreateUserComponent } from './views/create-user/create-user.component';
 import { AdminPageComponent } from './views/admin-page/admin-page.component';
+import { LogisticsManagerModule } from './views/logistics-manager/logistics-manager.module';
+import { NetworkComponent } from './views/network/network.component';
+import { ListPlannedRoutesComponent } from './views/list-planned-routes/list-planned-routes.component';
+import { GetPlannedRouteComponent } from './views/get-planned-route/get-planned-route.component';
+
 
 @NgModule({
   declarations: [
@@ -29,13 +28,10 @@ import { AdminPageComponent } from './views/admin-page/admin-page.component';
     LoginComponent,
     NetworkComponent,
     FleetManagerComponent,
-    CreateTruckComponent,
-    CreateRouteComponent,
-    LogisticsManagerComponent,
-    ListTruckComponent,
-    GetPlannedRouteComponent,
-    ListRoutesComponent,
     ListPlannedRoutesComponent,
+    GetPlannedRouteComponent,
+    CreateTruckComponent,
+    ListTruckComponent,
     TruckStatusComponent,
     CreateUserComponent,
     AdminPageComponent
@@ -49,7 +45,8 @@ import { AdminPageComponent } from './views/admin-page/admin-page.component';
       FormsModule,
       SharedModule,
       NgxPaginationModule,
-      WarehouseManagerModule
+      WarehouseManagerModule,
+      LogisticsManagerModule,
     ],
   providers: [],
   bootstrap: [AppComponent]
